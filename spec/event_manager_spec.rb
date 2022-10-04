127a6bd0e7339d7d8a33066f06ab858930af5877
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-04 08:28:48 +0100

    tests schema & empty class

---
+++ spec/event_manager_spec.rb
@@ -0,0 +1,27 @@
+describe EventManager do
+
+  describe '.subscribe' do
+    it 'should add a new handler' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe '.unsubscribe' do
+    it 'should remove the handler' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe '.broadcast' do
+    it 'should broadcast an event with no arguments' do
+      expect(true).to eq(false)
+    end
+    it 'should broadcast an event with one argument' do
+      expect(true).to eq(false)
+    end
+    it 'should broadcast an event with two arguments' do
+      expect(true).to eq(false)
+    end
+  end
+
+end
